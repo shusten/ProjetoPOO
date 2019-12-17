293b44882a2e80377345805186d930723608b06c
Luis Villas <shusten@hotmail.com>
2019-12-17 18:21:36 -0300

    Terminando chamando metodo avaliar na main

--- src/ProjetoVideo.java
+++ src/ProjetoVideo.java
@@ -11,8 +11,11 @@ public class ProjetoVideo {
 
         Visualizacao vis[] = new Visualizacao[5];
         vis[0] = new Visualizacao(g[0], v[2]);
+        vis[0].avaliar();
         System.out.println(vis[0].toString());
+
         vis[1] = new Visualizacao(g[0], v[1]);
+        vis[0].avaliar(35.0f);
         System.out.println(vis[1].toString());
 
 
